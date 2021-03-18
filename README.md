ca969abf52ebfc1a6c5c6dbf12632ffdbab5795c
Karolina <ksenkow@luc.edu>
2021-03-18 18:11:11 -0500

    update readme and fix dataframe structure

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ## Installation
     pip install pandas
     pip install sklearn
-    
+    pip install hyperopt
+    pip install hpsklearn
 
 #### To run symptom_predict.py, clone this repository:
     https://github.com/jjcolgan/group7Repo.git
--- symptom_predict.py
+++ symptom_predict.py
@@ -1,9 +1,18 @@
 # import required libraries
 import pandas as pd
 from sklearn import svm
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from hpsklearn import HyperoptEstimator
+from hpsklearn import any_classifier
+from hpsklearn import any_preprocessing
+from hyperopt import tpe
 
 # read dataframe containing all data
+from sklearn.preprocessing import LabelEncoder
+
 df = pd.read_csv('transposedCo-occurenceDataWithOutcomes.csv')
 
 ''' create separate dataframes for control & each symptom 
