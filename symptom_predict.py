a74a84711b93bc233428f1cff0ab613125214116
Karolina <ksenkow@luc.edu>
2021-03-18 16:15:14 -0500

    fixed error

--- symptom_predict.py
+++ symptom_predict.py
@@ -1,5 +1,7 @@
 # import required libraries
 import pandas as pd
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
 
 # read dataframe containing all data
 df = pd.read_csv('transposedCo-occurenceDataWithOutcomes.csv')
@@ -10,24 +12,22 @@ df = pd.read_csv('transposedCo-occurenceDataWithOutcomes.csv')
 
 # control & UTI+
 UTI = df[df.iloc[:,0].str.contains("^(?:C|E)")]
-X_UTI = UTI.drop(['outcome'], axis=1).values
+X_UTI = UTI.drop(['PatientID','outcome'], axis=1).values
 y_UTI = UTI['outcome'].values
 
 # control & overactive bladder
 OAB = df[df.iloc[:,0].str.contains("^(?:C|OAB)")]
-X_OAB = OAB.drop(['outcome'], axis=1).values
+X_OAB = OAB.drop(['PatientID','outcome'], axis=1).values
 y_OAB = OAB['outcome'].values
 
 # control & urge urinary incontinence
 UUI = df[df.iloc[:,0].str.contains("^(?:C|UUI)")]
-X_UUI = UUI.drop(['outcome'], axis=1).values
+X_UUI = UUI.drop(['PatientID','outcome'], axis=1).values
 y_UUI = UUI['outcome'].values
 
 # control & stress urinary incontinence
 SUI = df[df.iloc[:,0].str.contains("^(?:C|SUI)")]
-X_SUI = SUI.drop(['outcome'], axis=1).values
+X_SUI = SUI.drop(['PatientID','outcome'], axis=1).values
 y_SUI = SUI['outcome'].values
 
-def cross_fold_validation():
-    pass
 
