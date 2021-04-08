5222e03c21b756389757fb97a3bd9f88a78e9a63
Karolina <ksenkow@luc.edu>
2021-04-08 14:29:43 -0500

    cleaned up code

--- symptom_predict.py
+++ symptom_predict.py
@@ -3,9 +3,11 @@ import pandas as pd
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler
+
+# create outfile
+output = open('outfile.txt','w')
 
 df = pd.read_csv('transposedCo-occurenceDataWithOutcomes.csv')
 features = df.columns.drop(['PatientID','outcome']).values
@@ -38,6 +40,7 @@ y_SUI = SUI['outcome'].values
 # 5 folds
 def random_forest_before_selection(X,y):
     result = cross_val_score(RandomForestClassifier(n_estimators=1000,random_state=1),X,y, cv=5)
+    print('Random forest classification before feature selection: ')
     print(result)
 
 ''' feature selection uses test_train_split instead of cross validation, since cross val
@@ -75,24 +78,12 @@ def random_forest_feature_select(X,y):
 
     # create new dataset with important features & run new cross validation
     X_important_train = sel_feat.transform(X_train)
-    print(cross_val_score(RandomForestClassifier(n_estimators=60,random_state=1, n_jobs=-1),X_important_train,y_train, cv=5))
-
-if __name__ == '__main__':
-    print('accuracy scores of random forest before feature selection:')
-    random_forest_before_selection(X_SUI,y_SUI)
-    print('accuracy scores of random forest after feature selection:')
-    random_forest_feature_select(X_SUI,y_SUI)
+    print(cross_val_score(RandomForestClassifier(n_estimators=1000,random_state=1, n_jobs=-1),X_important_train,y_train, cv=5))
 
 def SVR(X,y):
     clf = svm.SVC(C=1, random_state=10)
     scores = cross_val_score(clf, X, y, cv=5)
     print(scores)
-
-
-
-if __name__ == '__main__':
-    print('accuracy scores of SVR:')
-    SVR(X_SUI,y_SUI)
 
 ##KNN
 def knn(X,y):
@@ -119,6 +110,15 @@ def knn(X,y):
     print(result)
 
 if __name__ == '__main__':
+    print('accuracy scores of random forest before feature selection:')
+    random_forest_before_selection(X_UUI,y_UUI)
+    print('accuracy scores of random forest after feature selection:')
+    random_forest_feature_select(X_UUI,y_UUI)
+
+    print('accuracy scores of SVR:')
+    SVR(X_SUI,y_SUI)
+
+    print('KNN:')
     knn(X_UTI,y_UTI)
     knn(X_OAB,y_OAB)
     knn(X_UUI,y_UUI)
